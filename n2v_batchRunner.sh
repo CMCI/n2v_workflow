4a06714d8c8c59224c9090c45da0145d2a98d898
CMCI <32138585+CMCI@users.noreply.github.com>
2019-08-16 14:26:32 -0400

    Update n2v_batchRunner.sh

--- n2v_batchRunner.sh
+++ n2v_batchRunner.sh
@@ -1,9 +1,10 @@
 for folder in job_input/*
 do 
     echo $folder
-    xvfb-run ImageJ --ij2 --headless --console -macro batchSplitter_3.ijm "$folder"
+    xvfb-run ImageJ --ij2 --headless --console -macro batchSplitter.ijm "$folder"
 done 
 
+#If the workflow is running smoothly for you, you can uncomment line 8; this will save you some disk space
 #xvfb-run ImageJ --ij2 --headless --console -macro batchSplitter.ijm 'job_input/'
 
 #rm job_input/*.nd2
